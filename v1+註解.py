c2b52cf31736cf0397ed809d79af9a1dc724117b
William <dragon892695@gmail.com>
2022-05-31 10:30:48 +0800

    Update v1+註解.py
    換來源

--- v1+註解.py
+++ v1+註解.py
@@ -111,7 +111,7 @@ priceLst_num = 0
 
 
 # 匯入歷史價格
-historicalData = historical_data('6235')
+historicalData = historical_data('1708')
 
 # 計算歷史均價
 moving_average_5day = sum(historicalData.price[-5:])/5
@@ -123,11 +123,18 @@ aver_5day_low = sum(historicalData.low)/5
 while True:
 
     # 取得現價
-    url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_6235.tw&json=1&delay=0&_=1653874012981"
-    req = requests.get(url)
-    data = req.json()
-    close = data["msgArray"][0]["z"]
+    #url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_2317.tw&json=1&delay=0&_=1653874012981"
+    #req = requests.get(url)
+    #data = req.json()
+    #close = data["msgArray"][0]["z"]
 
+    # Yahoo現價
+    res = requests.get('https://tw.stock.yahoo.com/_td-stock/api/resource/FinanceChartService.ApacLibraCharts;symbols=%5B%221708.TW%22%5D;type=tick?bkt=%5B%22tw-qsp-exp-no2-1%22%2C%22test-es-module-production%22%2C%22test-portfolio-stream%22%5D&device=desktop&ecma=modern&feature=ecmaModern%2CshowPortfolioStream&intl=tw&lang=zh-Hant-TW&partner=none&prid=2h3pnulg7tklc&region=TW&site=finance&tz=Asia%2FTaipei&ver=1.2.902&returnMeta=true')
+
+    jd = res.json()['data']
+    q = jd[0]['chart']['indicators']['quote'][0]['close']
+
+    close = q[-1]
     # 輸出更新價格(測試用，正式上線可以拿掉)
     print(close)
 
