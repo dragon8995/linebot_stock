2991e7e9749ceadf87133585ad6df3b5cb8caa0e
William <dragon892695@gmail.com>
2022-06-01 09:48:46 +0800

    Update app.py
    2603

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ priceLst_num = 0
 
 
 # 匯入歷史價格
-historicalData = historical_data('1708')
+historicalData = historical_data('2603')
 
 # 計算歷史均價
 moving_average_5day = sum(historicalData.price[-5:])/5
@@ -202,7 +202,7 @@ while True:
     #close = data["msgArray"][0]["z"]
 
     # Yahoo現價
-    res = requests.get('https://tw.stock.yahoo.com/_td-stock/api/resource/FinanceChartService.ApacLibraCharts;symbols=%5B%221708.TW%22%5D;type=tick?bkt=%5B%22tw-qsp-exp-no2-1%22%2C%22test-es-module-production%22%2C%22test-portfolio-stream%22%5D&device=desktop&ecma=modern&feature=ecmaModern%2CshowPortfolioStream&intl=tw&lang=zh-Hant-TW&partner=none&prid=2h3pnulg7tklc&region=TW&site=finance&tz=Asia%2FTaipei&ver=1.2.902&returnMeta=true')
+    res = requests.get('https://tw.stock.yahoo.com/_td-stock/api/resource/FinanceChartService.ApacLibraCharts;symbols=%5B%2603.TW%22%5D;type=tick?bkt=%5B%22tw-qsp-exp-no2-1%22%2C%22test-es-module-production%22%2C%22test-portfolio-stream%22%5D&device=desktop&ecma=modern&feature=ecmaModern%2CshowPortfolioStream&intl=tw&lang=zh-Hant-TW&partner=none&prid=2h3pnulg7tklc&region=TW&site=finance&tz=Asia%2FTaipei&ver=1.2.902&returnMeta=true')
 
     jd = res.json()['data']
     q = jd[0]['chart']['indicators']['quote'][0]['close']
